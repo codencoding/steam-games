93004e2f016eed50b81806cee32cb9468e9ddbd2
codencoding <36287664+Loegrath@users.noreply.github.com>
2021-01-14 16:16:13 -0800

    Fixed run_sys_req_scraper

--- run_sys_req_scraper.py
+++ run_sys_req_scraper.py
@@ -1,12 +1,19 @@
 import sys
-from src.scraping import SteamScraper
+from src.scraping.scraping import SteamScraper
 
-def run_sys_req_scraper(kwargs):
+def run_sys_req_scraper(**kwargs):
     ROOT_DIR = "./"
     sc = SteamScraper(ROOT_DIR)
     sc.scrape_data()
-    sc.collect_sys_reqs(kwargs)
+    print(kwargs)
+    sc.collect_sys_reqs(chunksize=kwargs.get("chunksize"), starting_chunk=kwargs.get("starting_chunk"), max_chunk=kwargs.get("max_chunk"))
 
 if __name__ == "__main__":
-    print("Running scraper with params:\n{}".format(sys.argv))
-    run_sys_req_scraper(sys.argv)
+    kwargs = {}
+    for elem in sys.argv[1:]:
+        key, val = elem.split('=')
+        kwargs[key] = int(val)
+    
+    print("Running scraper with params:\n{}".format(kwargs))
+    # run_sys_req_scraper(kwargs)
+    run_sys_req_scraper(chunksize=kwargs.get("chunksize"), starting_chunk=kwargs.get("starting_chunk"), max_chunk=kwargs.get("max_chunk"))
